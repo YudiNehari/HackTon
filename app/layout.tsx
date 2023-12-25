62fffbafbea425ef4fceb03c1341f8a5c87e9a88
YudiNehari <yehudasrugim@gmail.com>
2023-12-25 20:30:55 +0200

    Add bcryptjs and jsonwebtoken dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,138 +1,158 @@
 'use client'
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation'
-import { AppBar, Toolbar, Button, Box, Modal, TextField, Typography, Fade } from '@mui/material';
-import { Inter } from 'next/font/google'
-import './globals.css'
+import React, { createContext, useContext, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { AppBar, Toolbar, Button, Box, Modal, TextField, Typography, Fade, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+const UserContext:any = createContext(null);
 
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [user, setUser]:any = useState(null);
 
-  const router = useRouter()
+  const router = useRouter();
 
   const [openRegister, setOpenRegister] = useState(false);
   const [openSignIn, setOpenSignIn] = useState(false);
+  const [userType, setUserType] = useState('volunteer');
 
   const handleOpenRegister = () => setOpenRegister(true);
   const handleCloseRegister = () => setOpenRegister(false);
   const handleOpenSignIn = () => setOpenSignIn(true);
   const handleCloseSignIn = () => setOpenSignIn(false);
+  const handleUserTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserType(event.target.value);
+  };
 
-  // Dummy submit handler
-  const handleSubmit = (event: any) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-          email: data.get('email'),
-          password: data.get('password'),
-      });
-      setOpenSignIn(false);
-      
-      const response = fetch('http://localhost:3000/api/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              email: data.get('email'),
-              password: data.get('password'),
-          }),
-      });
-      
-      // volunteer, donor, organization
-      router.push('/volunteer');
-      
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    setOpenRegister(false);
+    setOpenSignIn(false);
+    setUser(Object.fromEntries(data.entries()));
+    localStorage.removeItem('user');
+    localStorage.setItem('user', JSON.stringify(Object.fromEntries(data.entries())));    
+  };
+
+  const Reg = () => (
+    <>
+    <TextField label="Name" name='name' fullWidth margin="normal" required />
+    <TextField label="Email" name='email' fullWidth margin="normal" required />
+    <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
+    <TextField label="Age" name='age' type="number" fullWidth margin="normal" required />
+    </>
+  )
+
+  const renderUserTypeFields = () => {
+    switch (userType) {
+      case 'volunteer':
+        return (
+          <>
+            <Reg />
+            <TextField label="Preferred Volunteer Work" name='volunteerWork' fullWidth margin="normal" required />
+            {/* Additional fields specific to volunteers */}
+          </>
+        );
+      case 'donor':
+        return (
+          <>
+            <Reg />
+            <TextField label="Donation Type" name='donationType' fullWidth margin="normal" required />
+            {/* Additional fields specific to donors */}
+          </>
+        );
+      case 'organization':
+        return (
+          <>
+            <TextField label="Organization Name" name='organizationName' fullWidth margin="normal" required />
+            <TextField label="Organization Type" name='organizationType' fullWidth margin="normal" required />
+            <TextField label="Organization Address" name='organizationAddress' fullWidth margin="normal" required />
+            <TextField label="Organization Phone Number" name='organizationPhoneNumber' fullWidth margin="normal" required />
+            <TextField label="Organization Email" name='organizationEmail' fullWidth margin="normal" required />
+            <TextField label="Organization Website" name='organizationWebsite' fullWidth margin="normal" required />
+            <TextField label="Organization Description" name='organizationDescription' fullWidth margin="normal" required />
+            <TextField label="Organization Name" name='organizationName' fullWidth margin="normal" required />
+          </>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <Box sx={{
-            flexGrow: 1,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Button color="inherit">
-                      <Link href="/">Home</Link>
-                    </Button>
-                    <Button color="inherit" onClick={handleOpenRegister}>Register</Button>
-                    <Button color="inherit" onClick={handleOpenSignIn}>Sign In</Button>
-                    <Button color="inherit">
-                      <Link href="/volunteer">volunteer</Link>
-                    </Button>
-                    <Button color="inherit">
-                      <Link href="/donor">donor</Link>
-                    </Button>
-                    <Button color="inherit">
-                      <Link href="/organization">organization</Link>
-                    </Button>
-                </Toolbar>
-            </AppBar>
+      <body>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Button color="inherit">
+                <Link href="/">Home</Link>
+              </Button>
+              <Button color="inherit" onClick={handleOpenRegister}>Register</Button>
+              <Button color="inherit" onClick={handleOpenSignIn}>Sign In</Button>
+            </Toolbar>
+          </AppBar>
 
-            <Modal
-              open={openRegister}
-              onClose={handleCloseRegister}
-              closeAfterTransition
-              BackdropProps={{ timeout: 500 }}
-            >
-                <Fade in={openRegister}>
-                    <Box sx={{
-                        width: '40%',
-                        margin: 'auto',
-                        marginTop: '10vh',
-                        padding: '20px',
-                        backgroundColor: 'white',
-                        borderRadius: '10px',
-                        boxShadow: 24,
-                    }}>
-                        <Typography variant="h6">Register</Typography>
-                        <form onSubmit={handleSubmit}>
-                            <TextField label="Email" name='email' fullWidth margin="normal" required />
-                            <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
-                            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>Register</Button>
-                        </form>
-                    </Box>
-                </Fade>
-            </Modal>
+          <Modal open={openRegister} onClose={handleCloseRegister} closeAfterTransition>
+            <Fade in={openRegister}>
+              <Box sx={{
+                width: '40%',
+                maxHeight: '80vh',
+                overflowY: 'auto',
+                margin: 'auto',
+                marginTop: '5vh',
+                padding: '20px',
+                backgroundColor: 'white',
+                borderRadius: '10px',
+                boxShadow: 24,
+              }}>
+                <Typography variant="h6">Register</Typography>
+                <form onSubmit={handleSubmit}>
+                  <FormControl component="fieldset" margin="normal">
+                    <FormLabel component="legend">Register as</FormLabel>
+                    <RadioGroup row value={userType} onChange={handleUserTypeChange}>
+                      <FormControlLabel value="volunteer" control={<Radio />} label="Volunteer" />
+                      <FormControlLabel value="donor" control={<Radio />} label="Donor" />
+                      <FormControlLabel value="organization" control={<Radio />} label="Organization" />
+                    </RadioGroup>
+                  </FormControl>
+                  {renderUserTypeFields()}
+                  <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>Register</Button>
+                </form>
+              </Box>
+            </Fade>
+          </Modal>
 
-            <Modal
-              open={openSignIn}
-              onClose={handleCloseSignIn}
-              closeAfterTransition
-              BackdropProps={{ timeout: 500 }}
-            >
-                <Fade in={openSignIn}>
-                    <Box sx={{
-                        width: '40%',
-                        margin: 'auto',
-                        marginTop: '10vh',
-                        padding: '20px',
-                        backgroundColor: 'white',
-                        borderRadius: '10px',
-                        boxShadow: 24,
-                    }}>
-                        <Typography variant="h6">Sign In</Typography>
-                        <form onSubmit={handleSubmit}>
-                            <TextField label="Email" name='email' fullWidth margin="normal" required />
-                            <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
-                            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>Sign In</Button>
-                        </form>
-                    </Box>
-                </Fade>
-            </Modal>
+          <Modal open={openSignIn} onClose={handleCloseSignIn} closeAfterTransition>
+            <Fade in={openSignIn}>
+              <Box 
+                sx={{ 
+                  width: '40%',
+                  maxHeight: '80vh',
+                  overflowY: 'auto',
+                  margin: 'auto',
+                  marginTop: '5vh',
+                  padding: '20px',
+                  backgroundColor: 'white',
+                  borderRadius: '10px',
+                  boxShadow: 24,
+                }}>
+                <Typography variant="h6">Sign In</Typography>
+                <form onSubmit={handleSubmit}>
+                  <TextField label="Email" name='email' fullWidth margin="normal" required />
+                  <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
+                  <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>Sign In</Button>
+                </form>
+              </Box>
+            </Fade>
+          </Modal>
         </Box>
-        {children}
+        <UserContext.Provider value={{ user, setUser }}>
+          {children}
+        </UserContext.Provider>
       </body>
     </html>
-  )
+  );
 }
+
+export const useUser = () => useContext(UserContext);
