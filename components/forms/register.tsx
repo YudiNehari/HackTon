3189461f2389ac0f6e2a0db232c05c023eb4c953
1@2.com <1@m.com>
2023-12-27 14:38:44 +0200

    n

--- components/forms/register.tsx
+++ components/forms/register.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Box, Typography, TextField, Button, Fade } from '@mui/material';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface RegisterComponentProps {
   open: boolean;
@@ -7,21 +9,60 @@ interface RegisterComponentProps {
 }
 
 const RegisterComponent: React.FC<RegisterComponentProps> = ({ open, handleClose }) => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
+  const Router = useRouter();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     // log form
     const formData = new FormData(event.currentTarget);
     const formValues = Object.fromEntries(formData.entries());
     console.log('form', formValues);
-    
-    handleClose();
+
+    try {
+      // Assuming your registration endpoint is '/api/register', adjust it accordingly
+      const isSuccess = (Math.random() > 0.5);
+      console.log(isSuccess);
+      
+      let response = {};
+      if(isSuccess){    
+        response = { data: { user_id: 123 } };
+      }else{ 
+        response = await axios.post('/api/register', formValues);
+      }
+        // @ts-ignore
+
+      if (response && response.data) {
+        // @ts-ignore
+        // Assuming the response has a 'user_id' property
+        const { user_id } = response.data;
+  
+        console.log('Registration successful. User ID:', user_id);
+        // @ts-ignore
+          sessionStorage.setItem('user_id', user_id);
+          Router.push('choose');
+          setError(null);
+          setSuccess(true);
+        }
+  
+      // Optionally, you can perform actions based on the successful registration
+      // For example, redirect the user to a new page or show a success message
+
+      // Close the modal or perform other actions
+      handleClose();
+    } catch (error) {
+      console.error('Registration failed:');
+      // Handle registration failure, show an error message, etc.
+    }
   };
 
   return (
     <Modal open={open} onClose={handleClose} closeAfterTransition>
       <Fade in={open}>
-        <Box 
-          sx={{ 
+        <Box
+          sx={{
             width: '40%',
             maxHeight: '80vh',
             overflowY: 'auto',
@@ -32,11 +73,17 @@ const RegisterComponent: React.FC<RegisterComponentProps> = ({ open, handleClose
             borderRadius: '10px',
             boxShadow: 24,
           }}>
-          <Typography variant="h6">Sign In</Typography>
+          <Typography variant="h6">Register</Typography>
           <form onSubmit={handleSubmit}>
-            <TextField label="Email" name='email' fullWidth margin="normal" required />
-            <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
-            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>Register</Button>
+            <TextField label="First Name" name="first_name" fullWidth margin="normal" required />
+            <TextField label="Last Name" name="last_name" fullWidth margin="normal" required />
+            <TextField label="Email" name="email" fullWidth margin="normal" required />
+            <TextField label="Password" name="password" type="password" fullWidth margin="normal" required />
+            <TextField label="Password Again" name="password_again" type="password" fullWidth margin="normal" required />
+            <TextField label="Phone Number" name="phone_number" fullWidth margin="normal" required />
+            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>
+              Register
+            </Button>
           </form>
         </Box>
       </Fade>
