97e6248c5b329293aad4a75f620d39c1b23a180d
1@2.com <1@m.com>
2023-12-27 15:03:25 +0200

    qwr

--- components/forms/bussinessForm.tsx
+++ components/forms/bussinessForm.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import { Modal, Box, Typography, TextField, Button, Fade } from '@mui/material';
+import axios from 'axios';
 
 interface BussinessFormComponentProps {
   open: boolean;
   handleClose: () => void;
+  userId: number; // Assuming you have a way to get the user ID from the parent component
 }
 
-const BussinessFormComponent: React.FC<BussinessFormComponentProps> = ({ open, handleClose }) => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+const BussinessFormComponent: React.FC<BussinessFormComponentProps> = ({ open, handleClose, userId }) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const formValues = Object.fromEntries(formData.entries());
-    console.log('form', formValues);
-    
 
-    
+    try {
+      // Assuming your register_business endpoint is '/api/register_business', adjust it accordingly
+      const response = await axios.post('/api/register_business', {
+        ...formValues,
+        user_id: userId,
+      });
 
-    console.log('BussinessFormComponent');
-    
-    handleClose();
+      // Assuming the response has a 'business_id' property
+      const { business_id } = response.data;
+
+      console.log('Business registration successful. Business ID:', business_id);
+
+      // Optionally, you can perform actions based on the successful business registration
+      // For example, redirect the user to a new page or show a success message
+
+      handleClose();
+    } catch (error) {
+      console.error('Business registration failed:');
+      // Handle registration failure, show an error message, etc.
+    }
   };
 
   return (
     <Modal open={open} onClose={handleClose} closeAfterTransition>
       <Fade in={open}>
-        <Box 
-          sx={{ 
+        <Box
+          sx={{
             width: '40%',
             maxHeight: '80vh',
             overflowY: 'auto',
@@ -36,11 +51,13 @@ const BussinessFormComponent: React.FC<BussinessFormComponentProps> = ({ open, h
             borderRadius: '10px',
             boxShadow: 24,
           }}>
-          <Typography variant="h6">BussinessFormComponentProps</Typography>
+          <Typography variant="h6">Register Business</Typography>
           <form onSubmit={handleSubmit}>
-            <TextField label="Email" name='email' fullWidth margin="normal" required />
-            <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
-            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>BussinessFormComponentProps</Button>
+            <TextField label="Business Name" name="business_name" fullWidth margin="normal" required />
+            <TextField label="City" name="city" fullWidth margin="normal" required />
+            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>
+              Register Business
+            </Button>
           </form>
         </Box>
       </Fade>
--- components/forms/create-activity.tsx
+++ components/forms/create-activity.tsx
@@ -1,27 +1,52 @@
 import React from 'react';
-import { Modal, Box, Typography, TextField, Button, Fade } from '@mui/material';
+import { Modal, Box, Typography, TextField, Button, Fade, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import axios from 'axios';
 
 interface CreateActivityComponentProps {
   open: boolean;
   handleClose: () => void;
+  businessId: number; // Assuming you have a way to get the business ID from the parent component
 }
 
-const CreateActivityComponent: React.FC<CreateActivityComponentProps> = ({ open, handleClose }) => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+const CreateActivityComponent: React.FC<CreateActivityComponentProps> = ({ open, handleClose, businessId }) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // log form
     const formData = new FormData(event.currentTarget);
     const formValues = Object.fromEntries(formData.entries());
-    console.log('form', formValues);
-    
-    handleClose();
+
+    try {
+      // Assuming your add_activity endpoint is '/api/add_activity', adjust it accordingly
+      const response = await axios.post('/api/add_activity', {
+        ...formValues,
+        business_id: businessId,
+      });
+
+      // Assuming the response has a 'success' property
+      const { success } = response.data;
+
+      if (success) {
+        console.log('Activity creation successful.');
+        // Optionally, you can perform actions based on the successful activity creation
+        // For example, redirect the user to a new page or show a success message
+      } else {
+        console.error('Activity creation failed.');
+        // Optionally, handle the case where the activity creation was not successful
+      }
+
+      // Close the modal or perform other actions
+      handleClose();
+    } catch (error) {
+      console.error('Error creating activity:');
+      // Handle the error, show an error message, etc.
+    }
   };
 
   return (
     <Modal open={open} onClose={handleClose} closeAfterTransition>
       <Fade in={open}>
-        <Box 
-          sx={{ 
+        <Box
+          sx={{
             width: '40%',
             maxHeight: '80vh',
             overflowY: 'auto',
@@ -32,11 +57,21 @@ const CreateActivityComponent: React.FC<CreateActivityComponentProps> = ({ open,
             borderRadius: '10px',
             boxShadow: 24,
           }}>
-          <Typography variant="h6">CreateActivityComponentProps</Typography>
+          <Typography variant="h6">Create Activity</Typography>
           <form onSubmit={handleSubmit}>
-            <TextField label="Email" name='email' fullWidth margin="normal" required />
-            <TextField label="Password" name='password' type="password" fullWidth margin="normal" required />
-            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>CreateActivityComponentProps</Button>
+            <TextField label="Date" name="date" fullWidth margin="normal" required type="date" />
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel>Type of Volunteer</InputLabel>
+              <Select label="Type of Volunteer" name="type_volunteer">
+                <MenuItem value="select">Select</MenuItem>
+                {/* Add other options based on your requirements */}
+              </Select>
+            </FormControl>
+            <TextField label="Number of Volunteers" name="NumberOfVolunteers" fullWidth margin="normal" required type="number" />
+            <TextField label="Activity Description" name="ActivityDescription" fullWidth multiline rows={4} margin="normal" required />
+            <Button type="submit" fullWidth variant="contained" sx={{ marginTop: '20px' }}>
+              Create Activity
+            </Button>
           </form>
         </Box>
       </Fade>
