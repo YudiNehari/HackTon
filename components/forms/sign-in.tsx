9f86a00aac2900b9d2a1b783b5d09441970e7945
1@2.com <1@m.com>
2023-12-27 14:14:58 +0200

    f

--- components/forms/sign-in.tsx
+++ components/forms/sign-in.tsx
@@ -19,36 +19,41 @@ const SignInComponent: React.FC<SignInComponentProps> = ({ open, handleClose }) 
     try {
       // Assuming your login endpoint is '/api/login', adjust it accordingly
       
-      const isSuccess = Math.random() > 0.5;
+      const isSuccess = (Math.random() > 0.5);
+      console.log(isSuccess);
       
-      
+      let response = {};
       if(isSuccess){    
-        const response = { data: { user_id: 123, business_id: 456 } };
+        response = { data: { user_id: 123, business_id: 456 } };
       }else{
         
-        const response = await axios.post('/api/login', formValues);
+        response = await axios.post('/api/login', formValues);
       }
       // Assuming the response has a 'user_id' and 'business_id' property
       
       // @ts-ignore
+      console.log(response,response.data,response && response.data);
+      
+      // @ts-ignore
       if (response && response.data) {
+      // @ts-ignore
         const { user_id, business_id } = response.data;
         console.log('Login successful. User ID:', user_id);
         console.log('Business ID:', business_id);
         sessionStorage.setItem('user_id', user_id);
         sessionStorage.setItem('business_id', business_id);
         Router.push('choose');
+        setError(null);
+        setSuccess(true);
       }
       // Save login information in the session
 
       // Reset error and set success to true
-      setError(null);
-      setSuccess(true);
 
       // Close the modal or perform other actions
       handleClose();
     } catch (error) {
-      console.error('Login failed:', error.message);
+      console.error('Login failed:');
 
       // Set error message and reset success
       setError('Login failed. Please check your credentials.');
